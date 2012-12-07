1737591dea5d69cea02316d7d15fa8a84d4f381d
Simon Käser <skaeser@raptus.com>
2012-12-07 14:12:20 +0000

    added nvd3 css file

--- src/js/nvd3/__init__.py
+++ src/js/nvd3/__init__.py
@@ -3,4 +3,5 @@ import js.d3
 
 library = Library('nvd3', 'resources')
 
-nvd3 = Resource(library, 'nv.d3.js', depends=[d3.d3], minified='nv.d3.min.js')
+css = Resource(library, 'nv.d3.css')
+nvd3 = Resource(library, 'nv.d3.js', depends=[js.d3.d3, css], minified='nv.d3.min.js')
